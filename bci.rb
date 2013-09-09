5d76ac1ea208ae21802d860b723e5cd0b04c50c8
anthropomorphic <anthropomorphic@mikefdorst.com>
2013-09-08 21:07:01 -0400

    Remove debug statements and comments

--- bci.rb
+++ bci.rb
@@ -1,49 +1,5 @@
-##
-# True if in debug mode
-#
-DEBUG = File.open("exec.debug", "w")
-#DEBUG = STDOUT
-#DEBUG = false
-
-##
-# Controls debug output for executing stage
-#
-DebugExec = true
-
-##
-# Padding for +debug:exec+ statements
-#
-DebugExecPad = " "*14
-
-##
-# Runs a block of code without warnings.
-#
-def silence_warnings(&block)
-  original_warn_level = $VERBOSE
-  $VERBOSE = nil
-  result = block.call
-  $VERBOSE = original_warn_level
-  result
-end
-
-##
-# Prevents any debugging if +DEBUG+ is not set
-#
-unless DEBUG
-  silence_warnings {
-    # run in silent mode becuase redefinition of constants will cause a warning.
-    DebugParse = DebugExec = false
-  }
-end
-
-##
-# Maintains the virtual machine which runs the bytecode.
-#
 module Runtime
   
-  ##
-  # The virtual machine which runs the bytecode.
-  #
   module VM
     @reg = [].fill(0, 0..15)
     def self.reg
@@ -55,37 +11,17 @@ module Runtime
     
   end
   
-  ##
-  # Contains methods corresponding to +bc+ instructions.
-  #
   module Code
-    ##
-    # Stores +val+ in +reg+.
-    # @param reg [Fixnum]
-    # @param val
-    #
     def self.store(arg)
-      DEBUG.puts DebugExecPad + "Storing #{arg[0]} in r#{arg[1]}" if DebugExec
       RT::VM.reg[arg[0]] = arg[1]
     end
     
-    ##
-    # Prints the value of register +reg+.
-    # @param reg [Fixnum]
-    #
     def self.debug(arg)
       p RT::VM.reg[arg[0]]
     end
     
-    ##
-    # Multiplies +lhr+ by +rhr+ and stores the result in +lhr+.
-    # @param lhr [Fixnum]
-    # @param rhr [Fixnum]
-    #
     def self.multiply(arg)
-      DEBUG.puts DebugExecPad + "Multiplying r#{arg[0]} (#{RT::VM.reg[arg[0]]}) x r#{arg[1]} (#{RT::VM.reg[arg[1]]})" if DebugExec
       RT::VM.reg[arg[0]] = RT::VM.reg[arg[0]] * RT::VM.reg[arg[1]]
-      DEBUG.puts DebugExecPad + "Result stored in r#{arg[0]} (#{RT::VM.reg[arg[0]]})" if DebugExec
     end
   end
   
@@ -99,51 +35,29 @@ module Runtime
   
 end
 
-##
-# Alias of +Runtime+
-#
 RT = Runtime
 
 module Interpreter
   
-  @insgr_code = nil if DebugExec
-  
   @instr = nil
   @args = []
   
-  ##
-  # Interprets a single line of +bc+.
-  #
   def self.interpret(byte)
-    
     if @instr == nil
       @instr = RT::InstrCode[byte]
-      @instr_code = byte if DebugExec
     else
       @args.push byte
     end
     
     if @instr.args == @args.length
-      if DebugExec
-        DEBUG.print "debug:exec: 0x%02x " % @instr_code
-        for arg in @args
-          DEBUG.print "0x%02x " % arg
-        end
-        DEBUG.puts
-      end
       @instr.code.call(@args)
       
       @instr = nil
       @args = []
-      
     end
-    
   end
 end
 
-##
-# Program driver.
-#
 if __FILE__ == $0
   ARGF.each_byte do |byte|
     Interpreter.interpret(byte)
--- bct.rb
+++ bct.rb
@@ -1,86 +1,32 @@
-##
-# Output streams
-#
 OUT = File.open("prog.bc", "w")
-DEBUG = File.open("prog.debug", "w")
-#DEBUG = STDOUT
-
-##
-# Controls debug output for parsing stage
-#
-DebugParse = false
-
-##
-# Controls debug output for encoding stage
-#
-DebugEncode = true
 
 module Translator
   
-  ##
-  # Padding for +debug:parse+ statements
-  #
-  DebugParsePad = "#"+" "*16
-  
-  ##
-  # Padding for +debug:encode+ statements
-  #
-  DebugEncodePad = "#"+" "*17
-  
-  ##
-  # Holds resources for preprocessing
-  #
   module Preprocessor
+    
     class Methods
-      ##
-      # Strips out comments.
-      # @param line [String]
-      # @param pos [Fixnum] the position of the start of the comment in +line+
-      # @return [String] the resulting line
+      
       def self.comment(line, pos)
         line[0, pos]
       end
     end
     
-    ##
-    # A +Hash<String, Method<String, Fixnum>>+ of key characters to be processed
-    #   before parsing.
-    #
-    # Keys should be single character +String+s.
-    #
-    # In the preprocess step, the line will be scanned for keys in +Keychars+.
-    # Upon locating one, the preprocessor will call the corresponding +Method+
-    #   for the key, passing in a +String+ representing the line,
-    #   and a +Fixnum+ representing the location at which the key was found.
-    #
     Keychars = {
       "#" => Methods.method(:comment)
     }
-    
   end
   
   module Encoder
     
-    ##
-    # A +Hash<String, String>+ of instruction names to be mapped to their
-    #   associated +bc+ values.
-    #
     Keywords = {
       "stor" => 0x01,
       "mult" => 0x13,
       "debug"=> 0x60
     }
-    
   end
   
-  ##
-  # Translates a single line of +bc+.
-  #
   def self.translate(line, line_num = nil)
     
-    ##
-    # Strips out comments.
-    #
     preprocess = lambda do
       index = 0
       line.each_char do |c|
@@ -91,36 +37,24 @@ module Translator
       end
     end
     
-    ##
-    # Splits up the line into tokens, then resolves them into raw data and
-    #   +bc+ instructions.
-    #
     parse = lambda do
-      DEBUG.puts DebugParsePad + "Split '#{line}'" if DebugParse
       line = line.split
-      DEBUG.puts DebugParsePad + "Result: " + line.inspect if DebugParse
-      DEBUG.puts DebugParsePad + "Resolve register names" if DebugParse
       (1..line.length-1).each do |i|
         if line.length > 1 and line[i][0] == "r"
           line[i] = Integer(line[i][1..-1])
         end
       end
-      DEBUG.puts DebugParsePad + "Result: " + line.inspect if DebugParse
-      DEBUG.puts DebugParsePad + "Resolve integer literals" if DebugParse
       (1..line.length-1).each do |i|
         line[i] = Integer(line[i])
       end
-      DEBUG.puts DebugParsePad + "Result: " + line.inspect if DebugParse
     end
     
     encode = lambda do
       for tok in line
         if tok.class == Fixnum
           OUT.print tok.chr
-          DEBUG.print "0x%02x " % tok if DebugEncode
         elsif tok.class == String
           OUT.print Encoder::Keywords[tok].chr
-          DEBUG.print "0x%02x " % Encoder::Keywords[tok] if DebugEncode
         end
       end
     end
@@ -129,20 +63,13 @@ module Translator
     begin
       preprocess.()
       
-      DEBUG.puts "# debug:parse:" + line_num.to_s + ": '" + line + "'" if DebugParse
       parse.()
-      DEBUG.puts if DebugParse
       
-      #DEBUG.puts "# debug:encode:" + line_num.to_s + ": #{line.inspect}" if DebugEncode
       encode.()
-      1.times {DEBUG.puts} if DebugEncode and line.length > 0
     end
   end
 end
 
-##
-# Program driver.
-#
 if __FILE__ == $0
   iter = 0
   ARGF.each_line do |line|
